167b683d6eba8913a778254255fb10789519fd83
Igor_riabikov <igor.riabikov@outlook.com>
2021-01-26 21:27:35 +0200

    init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
+sudo cp -r /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/
+sudo nginx -s reload
